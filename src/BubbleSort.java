d9c048c178269c2c6bb2d18017b1bc34e3b2959a
Leonardo Delmiro <leodelmiroms@gmail.com>
2020-05-02 21:40:04 -0300

    arrayChar to BubbleSort

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -13,4 +13,16 @@ public class BubbleSort {
         }
     }
 
+    public static void bubbleSort(char[] array) {
+        char aux;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i+1; j < array.length; j++) {
+                if (array[i] > array[j]) {
+                    aux = array[i];
+                    array[i] = array[j];
+                    array[j] = aux;
+                }
+            }
+        }
+    }
 }
--- test/BubbleSortTest.java
+++ test/BubbleSortTest.java
@@ -4,16 +4,22 @@ import static org.junit.Assert.*;
 
 public class BubbleSortTest {
     int[] array;
+    char[] arrayChar;
+
 
     @Before
     public void init() {
         array = new int[]{5, 3, 1, 2, 4};
+        arrayChar = new char[]{'c', 'e', 'a', 'd', 'b'};
     }
 
     @Test
     public void testSort() {
         int[] expected = new int[]{1, 2, 3, 4, 5};
+        char[] expectedChar = new char[]{'a', 'b', 'c', 'd', 'e'};
         BubbleSort.bubbleSort(array);
+        BubbleSort.bubbleSort(arrayChar);
         assertArrayEquals(expected, array);
+        assertArrayEquals(expectedChar, arrayChar);
     }
 }
